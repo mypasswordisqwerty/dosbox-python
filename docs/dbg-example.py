c951a045511d2831dcb0a1e24530ca997122dbce
Samuli Tuomola <samuli.tuomola@gmail.com>
2011-05-08 00:32:24 +0300

    Updated to use new features

--- docs/dbg-example.py
+++ docs/dbg-example.py
@@ -8,37 +8,56 @@
 from dosboxdbg import *
 import time
 
+def msg(m):
+	ShowMsg('[PY] '+m)
+
 def hexdump(src, length=16):
     result=[]
     for i in xrange(0, len(src), length):
        s = src[i:i+length]
        hexa = b' '.join(["%02X"%ord(x) for x in s])
        text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.'  for x in s])
-       result.append("%04X   %-*s   %s\n" % (i, length*3, hexa, text))
+       result.append("%-*s %s\n" % (length*3, hexa, text))
     return ''.join(result)
 
-def tick():
-	ShowMsg('tick %s' % time.ctime())
-	UnregisterTick(tick)
-
-def log(tick, logger, msg):
-	if tick == 3 and logger == 'EXEC':
-		ShowMsg('=== AT %i GOT "%s", will demo' % (tick,msg))
-		demo()
-
 def demo():
-	regs = (eax,ebx,ecx,edx, esi,edi, esp,ebp,eip) = GetRegs()
-	ShowMsg( 'registers: '+str(regs) )
-	ShowMsg( 'executing: '+disasm(0xF000, eip, eip) )
-	m = ReadMem(0xC843, 0x061E, 12)
-	ShowMsg( 'some mem: '+hexdump(m) )
-	EnableDebugger()
-	ParseCommand("BP ")
-	for h in GetBpoints():
-		ShowMsg('BP %i. %x:%x' % (h.GetIntNr(), h.GetSegment(), h.GetOffset()))
-		#ShowMsg(str(dir(h)))
+	try:
+		regs = (eax,ebx,ecx,edx, esi,edi,esp,ebp,eip) = GetRegs()
+		segs = (cs,ds,es,fs,gs,ss) = GetSegments()
+		msg( 'registers: '+str(regs) )
+		msg( 'segments: '+str(segs) )
+		msg( 'executing: '+disasm(0xF000, eip, eip) )
+		msg( 'display mode: %x' % VgaMode() )
+		m = ReadMem(GetAddress(0x0, 0x1200), 14)
+		msg( 'some mem: '+hexdump(m) )
+		ParseCommand("BP ")
+		for h in GetBpoints():
+			msg('BP %i. %x:%x' % (h.GetIntNr(), h.GetSegment(), h.GetOffset()))
+			#msg(str(dir(h)))
+		msg('F5 to continue')
+	except Exception as e:
+		msg(e)
 
-ShowMsg("HELLO WORLD")
-RegisterTick(tick)
-RegisterLog(log)
+def shift(seq, n=1):
+	return seq[n:]+seq[:n]
+
+def rotpal():
+	pal = GetPalette()
+	SetPalette( shift(pal,3) )
+	if time.time() - starttime > 0.5:
+		DontListenFor(VSYNC, rotpal)
+
+def executed(binhash):
+	msg('executed binary hash: %x' % binhash)
+
+def logged(tick, logger, mesg):
+	if logger == 'VGA' and 'normal' in mesg:
+		msg('Hello, press alt-break for some python bindings')
+
+starttime = time.time()
+
+ListenFor(BREAK, demo)
+ListenForExec(executed)
+ListenForLog(logged)
+ListenFor(VSYNC, rotpal)
 
